caf1e917633bf8b7277058368f8fe8ed8e821d1c
Muchiri <waburimuchiri@gmail.com>
2025-04-24 01:04:45 +0300

    feat: add UI components and hooks
    - Introduced Skeleton component for loading states.
    - Added Slider component with customizable range.
    - Implemented Sonner for toast notifications.
    - Created Switch component for toggle functionality.
    - Developed Table component with header, body, footer, and cell subcomponents.
    - Added Tabs component with triggers and content.
    - Implemented Textarea component for user input.
    - Created ToggleGroup and Toggle components for grouped toggle functionality.
    - Added Tooltip component for displaying additional information on hover.
    - Implemented useIsMobile hook for mobile detection.
    - Added utility function for class name merging.
    - Created QueryProvider for React Query integration.
    - Implemented ThemeProvider for theme management.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "@/providers/theme-provider";
+import QueryProvider from "@/providers/query-provider";
 
-const geistSans = Geist({
+const sans = Manrope({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const mono = JetBrains_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,9 +28,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sans.variable} ${mono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
+        <Toaster />
       </body>
     </html>
   );
