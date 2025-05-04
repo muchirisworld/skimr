f412b0a916ce002bcea3c274c1e7031db8e4a0f6
Muchiri <waburimuchiri@gmail.com>
2025-05-04 03:39:05 +0300

    fix: correct database client configuration by disabling SSL

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -7,5 +7,5 @@ import * as schema from "./schema";
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) throw new Error('DATABASE_URL is missing');
 
-const client = postgres(connectionString, { ssl: true });
+const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema });
